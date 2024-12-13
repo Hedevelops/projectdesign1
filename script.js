622ca34330e09c45743f36eeffc863986470af8d
Hedevelops <udaysinghy7@gmail.com>
2024-12-13 17:56:49 +0530

    commit 1 git commit --amend --author Hedevelops udaysinghy7@gmail.com

---
+++ script.js
@@ -0,0 +1,10 @@
+function ham(){
+    document.querySelector("#slide").classList.remove("invisible");
+    document.querySelector("#hambr").classList.add("invisible");
+
+}
+function cross(){
+    document.querySelector("#slide").classList.add("invisible");
+    document.querySelector("#hambr").classList.remove("invisible");
+
+}
